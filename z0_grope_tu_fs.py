6e40c92ba0e5e28f04d65f672ab9e9d2862b03ef
fsonak <fredericsonak04@gmail.com>
2022-03-14 18:47:02 +0100

    initial commit adding grope data adding grope data analysis python file adding grope data analysis jupyter notebook

---
+++ z0_grope_tu_fs.py
@@ -0,0 +1,75 @@
+import numpy as np
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import uproot
+import awkward as ak
+import mplhep
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+path_to_base_dir = 'Data/'
+
+hadron_df = pd.read_csv(path_to_base_dir+'Z_experiment-hadron.csv')
+electron_df = pd.read_csv(path_to_base_dir+'Z_experiment-elektron.csv')
+myon_df = pd.read_csv(path_to_base_dir+'Z_experiment-myon.csv')
+tauon_df = pd.read_csv(path_to_base_dir+'Z_experiment-tauon.csv')
+
+print(hadron_df)
+
+
+ecal_sum_e_string = 'ECAL(SumE)'
+hcal_sum_e_string = 'HCAL(SumE)'
+
+#TODO die anderen beiden namen einfügen
+Pcharged_string = 'Ctrk(SumP)'
+Ncharged_string = 'Ctrk(N)'
+
+
+# plt.style.use(mplhep.style.ATLAS) # You can load ATLAS/CMS/ALICE plot style
+
+plt.hist(hadron_df[ecal_sum_e_string], histtype='step', bins=20, color='C1', label='hadron')
+plt.hist(electron_df[ecal_sum_e_string], histtype='step',bins=20, color='C2', label='electron')
+plt.hist(myon_df[ecal_sum_e_string], histtype='step',bins=20, color='C3', label='myon')
+plt.hist(tauon_df[ecal_sum_e_string], histtype='step',bins=20, color='C4', label='tauon')
+plt.title('E_ecal')
+plt.ylabel('Counts')
+plt.xlabel('E_ecal [GeV]')
+plt.legend()
+plt.show()
+
+plt.hist(hadron_df[hcal_sum_e_string], histtype='step', bins=10, range=(0,50), color='C1', label='hadron')
+plt.hist(electron_df[hcal_sum_e_string], histtype='step',bins=4, color='C2', label='electron')
+plt.hist(myon_df[hcal_sum_e_string], histtype='step',bins=20, color='C3', label='myon')
+plt.hist(tauon_df[hcal_sum_e_string], histtype='step',bins=20, color='C4', label='tauon')
+plt.title('E_hcal')
+plt.ylabel('Counts')
+plt.xlabel('E_hcal [GeV]')
+plt.legend()
+plt.show()
+
+plt.hist(hadron_df[Pcharged_string], histtype='step', bins=10,range=(0,110), color='C1', label='hadron')
+plt.hist(electron_df[Pcharged_string], histtype='step',bins=4, color='C2', label='electron')
+plt.hist(myon_df[Pcharged_string], histtype='step',bins=20, color='C3', label='myon')
+plt.hist(tauon_df[Pcharged_string], histtype='step',bins=20, color='C4', label='tauon')
+plt.title('Pcharged')
+plt.ylabel('Counts')
+plt.xlabel('Pcharged [GeV]')
+plt.legend()
+plt.show()
+
+plt.hist(hadron_df[Ncharged_string], histtype='step', bins=10, range=(0,40), color='C1', label='hadron')
+plt.hist(electron_df[Ncharged_string], histtype='step',bins=4, color='C2', label='electron')
+plt.hist(myon_df[Ncharged_string], histtype='step',bins=20, color='C3', label='myon')
+plt.hist(tauon_df[Ncharged_string], histtype='step',bins=20, color='C4', label='tauon')
+plt.title('Ncharged')
+plt.ylabel('Counts')
+plt.xlabel('Ncharged')
+plt.legend()
+plt.show()
+
+print('done')
