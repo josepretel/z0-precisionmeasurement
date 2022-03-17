70f703e0f4eb44bbac83a1701defd3a8b14d8fcf
Simon Beyer <simon.beyer@hotmail.de>
2022-03-17 18:02:59 +0100

    calculated efficiency matrix

--- functions.py
+++ functions.py
@@ -2,26 +2,23 @@
 import numpy as np
 import awkward as ak
 
-def get_efficiency(data,channels,var,cuts):
+
+def get_efficiency(data,cuts,relevant_vars):
 	efficiency_matrix = np.zeros((4,4))
-#	  efficiency matrix, e.g.
-#		      ee	 mm 	qq       tt
-#		ee     .         .       .       .
-#		mm     .         .       .       .
-#		qq     .         .       .       .
-#		tt     .         .       .       .
-		
-		
-	for i in np.arange(len(channels)):
-		for j in np.arange(len(channels)):
+	error_matrix = np.zeros((4,4))
+	
+	el={'ee': 0,'mm':1,'qq':2,'tt':3}
+	
+	### Calculate the efficiency matrix elements 
+	for channel_i,cuts_i in cuts.items():
+		for channel_j,rel_vars in relevant_vars[channel_i].items():
 			passed = 0
 			total = 0
-			for var,cut in cuts[channels[i]].items():
-				mask0 = data[channels[j]][var] >= cut[0]
-				mask1 = data[channels[j]][var] < cut[1]
+			for var in rel_vars:
+				mask0 = data[channel_j][var]>= cuts_i[var][0]
+				mask1 = data[channel_j][var] < cuts_i[var][1]
 				total +=len(mask0)
 				passed += sum(mask0)+sum(mask1)-len(mask0)
-			efficiency_matrix[i][j]=passed/total
+			efficiency_matrix[el[channel_i]][el[channel_j]]=passed/total
 				
-	
-	return efficiency_matrix
+	return efficiency_matrix,error_matrix
